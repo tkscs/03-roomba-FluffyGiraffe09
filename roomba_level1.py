77048828e376b44f341211e1cbdc12d788d29246
FluffyGiraffe09 <oroy2028@kehillahstudent.org>
2025-09-04 09:26:44 -0700

    Roomba levels 1 and 3

--- roomba_level1.py
+++ roomba_level1.py
@@ -2,7 +2,7 @@
 # Roomba in Python
 # This file implements an algorithm for a roomba cleaning a room.
 #
-# Author: Dr. EB <------ REPLACE THIS WITH YOUR NAME!
+# Author: Orli <------ REPLACE THIS WITH YOUR NAME!
 # -----------------------------------------------------------------------------
  
 from turtle import left, forward, backward
@@ -13,7 +13,7 @@ window = room.draw_room(level = 1)
 
 ###
 # Start your code here
- 
+
  
  
 # End your code here
